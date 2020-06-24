b132dc3da95b2ed7798a590098a17f593f1ea875
Gustavo Maedo <31759445+gustavomaedo@users.noreply.github.com>
2020-06-24 16:38:35 -0300

    Update ddns.py

--- ddns.py
+++ ddns.py
@@ -1,16 +1,19 @@
 import requests
 import time
-
+import datetime
 
 class DDNS():
+    iperror = 0
     first = True
     domains = []
-    ipr = 'http://whatismyip.akamai.com/'
+    ipr = 'https://domains.google.com/checkip'
+    ipr2 = 'http://whatismyip.akamai.com/'
+    ipr3 = 'https://api.ipify.org'
     dip = 'domains.google.com'
     iptime = 3600
 
-    myip = requests.get(ipr).text
-    lastip = myip
+    myip = ''
+    lastip = ''
 
     def set_domain(self, domain, user, password):
         value = {
@@ -27,6 +30,8 @@ class DDNS():
 
     def init(self):
         print('Starting ddns')
+        self.myip = self.request_ip()
+        self.lastip = self.myip
         if len(self.domains) > 0:
             while True:
                 self.get_ip()
@@ -36,18 +41,17 @@ class DDNS():
 
     def get_ip(self):
         if self.first:
+            print('\nTime: %s' % datetime.datetime.now())
             print('IP is %s' % self.myip)
             self.first = False
         else:
             while self.myip == self.lastip:
                 self.lastip = self.myip
-                self.myip = requests.get(self.ipr).text
+                self.myip = self.request_ip()
                 if self.myip != self.lastip:
+                    print('\nTime: %s' % datetime.datetime.now())
                     print('IP is %s' % self.myip)
-                if len(self.myip) > 18:
-                    print('IP error')
                 time.sleep(self.iptime)
-
 
     def write(self):
         for value in self.domains:
@@ -66,3 +70,20 @@ class DDNS():
                     print("Error on %s:\n%s" % (domain, response))
             else:
                 print('No changes on %s' % domain)
+
+    def request_ip(self):
+        if self.iperror == 1:
+            ipr = self.ipr2
+        elif self.iperror == 2:
+            ipr = self.ipr3
+        else:
+            ipr = self.ipr
+        try:
+            reqip = requests.get(ipr).text
+        except:
+            if self.iperror >= 2:
+                self.iperror = 0
+            else:
+                self.iperror += 1
+            reqip = 'request error\nChanging api'
+        return reqip
